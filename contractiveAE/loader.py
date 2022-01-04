4401999e3f21a72e26ef70e4d2329137285e759e
rutu1012 <rutushrirame@gmail.com>
2022-01-04 19:43:57 +0530

    Comments added

--- contractiveAE/loader.py
+++ contractiveAE/loader.py
@@ -4,26 +4,37 @@ from torchvision import datasets, transforms
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+
 def trainLoader(batch_size):
-    transform=transforms.ToTensor()
+    '''
+    This function loads the training MNIST dataset
+    :param batch_size: batch size to be used during training
+    :return: DataLoader object of the train set
+    '''
+    transform = transforms.ToTensor()
     train_data = datasets.MNIST(
         root="~/torch_datasets",
         train=True,
         download=True,
         transform=transform
     )
-    train_loader=DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     return train_loader
 
+
 def testLoader(batch_size):
-    transform=transforms.ToTensor()
+    '''
+    This function loads the testing MNIST dataset
+    :param batch_size: batch size to be used during testing
+    :return: DataLoader object of the test set
+    '''
+    transform = transforms.ToTensor()
     test_data = datasets.MNIST(
         root="~/torch_datasets",
         train=False,
         download=True,
         transform=transform
-    )  
-    test_loader=DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    )
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
     return test_loader
 
-# trainLoader,testLoader = trainLoader(600),testLoader(600)
