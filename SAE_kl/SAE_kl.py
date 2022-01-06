22a0d8b05fa28b2d95ce7b269b954fbaa7fc521b
Khdave02 <87975841+Khdave02@users.noreply.github.com>
2022-01-06 20:27:39 +0530

    Update SAE_kl.py

--- SAE_kl/SAE_kl.py
+++ SAE_kl/SAE_kl.py
@@ -4,9 +4,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import loader
+import loader  #importing mnist dataset
 from tqdm import tqdm
-seed = 1  # seed is used to ensure that we get the same output every time
+seed = 1  # seed is used to ensure to get the same output every time
 torch.manual_seed(seed)
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 train_loader = loader.trainLoader(100)
@@ -14,17 +14,20 @@ test_loader = loader.testLoader(600)
 lr =1e-3
 num_epochs=10
 class SparseAutoencoder(nn.Module):
+    ```
+     Sparse AE model 
+   ```
     def __init__(self):
         super().__init__()
-        self.enc1 = nn.Conv2d(1, 16, 3, stride=2, padding=1)
-        self.enc2 = nn.Conv2d(16, 32, 3, stride=2, padding=1)
-        self.enc3 = nn.Conv2d(32, 64, 7)
-        self.dec1 = nn.ConvTranspose2d(64, 32, 7)
-        self.dec2 = nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1)
-        self.dec3 = nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1)
+        self.enc1 = nn.Conv2d(1, 16, 3, stride=2, padding=1)  # -> N, 16, 14, 14
+        self.enc2 = nn.Conv2d(16, 32, 3, stride=2, padding=1) # -> N, 32, 7, 7
+        self.enc3 = nn.Conv2d(32, 64, 7)   # -> N, 64, 1, 1
+        self.dec1 = nn.ConvTranspose2d(64, 32, 7)    # -> N, 32, 7, 7
+        self.dec2 = nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1)  # N, 16, 14, 14 
+        self.dec3 = nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1)  # N, 1, 28, 28 
     def forward(self, x):
         # encoding
-        ret_val=[]
+        ret_val=[]     #ret_val is used to store activations
         x = F.relu(self.enc1(x))
         ret_val.append(x)
 
@@ -32,7 +35,7 @@ class SparseAutoencoder(nn.Module):
         ret_val.append(x)
         x = F.relu(self.enc3(x))
         ret_val.append(x)
-
+        #decoding
         x = F.relu(self.dec1(x))
         ret_val.append(x)
         x = F.relu(self.dec2(x))
@@ -49,6 +52,18 @@ def sparse_loss(model_children):
         loss += F.kl_div(torch.mean(i,dim=0), rho*torch.ones_like(i[0]))
     return beta*torch.abs(loss)
 def training(model,trainloader,train_output,epoch,optimizer,criterion):
+    '''
+        function trains the Neural Network.
+        Parameters-
+        model: CNN SparseAutoEncoder model
+        trainloader: DataLoader object that iterates through the train set
+        optimizer: Adam optimizer  to be used for training
+        criterion: MSE loss function to be used for training
+        train_output: list containing per epoch results(input & output) obtained after training
+        epoch: current epoch
+        returns train loss per epoch
+        
+    '''
     print("Training")
     loss = 0.0                  # Looping for every epoch
     for img,_ in tqdm(trainloader):  # Looping for every batch
@@ -62,6 +77,16 @@ def training(model,trainloader,train_output,epoch,optimizer,criterion):
     train_output.append((epoch, img, outputs))
     return loss
 def testing(model,testloader,test_output,epoch,criterion):
+    '''
+            function tests the trained model
+            Parameters-
+            model: CNN SparseAutoEncoder model
+            testloader: DataLoader object that iterates through the test set
+            criterion: MSE loss function to be used for testing
+            test_output: list containing per epoch results(input & output) obtained after testing
+            epoch: current epoch
+            returns test loss per epoch
+    '''
     print("Testing")
     with torch.no_grad():
             loss = 0.0
@@ -75,7 +100,7 @@ def testing(model,testloader,test_output,epoch,criterion):
     return loss
 def cost_graph(loss_list,title):
     plt.plot(loss_list)
-    plt.ylabel('cost')
+    plt.ylabel('loss')
     plt.xlabel('epochs')
     plt.title(title)
     plt.show()
@@ -106,22 +131,23 @@ def main():
     test_output = []
     train_loss_list = []
     test_loss_list = []
-    #training
+    
     for epoch in range(num_epochs):
+        #training
         train_loss = training(model,train_loader,train_output,epoch,optimizer,criterion)
         train_loss_list.append(train_loss)
         print("epoch : {}/{}, recon loss = {:.8f}".format(epoch + 1, num_epochs, train_loss))
-
+        #testing
         test_loss = testing(model,test_loader,test_output,epoch,criterion)
         test_loss_list.append(test_loss)
         print("epoch : {}/{}, recon loss = {:.8f}".format(epoch + 1, num_epochs, test_loss))
 
-    cost_graph(train_loss_list,title="CAE Train graph")
+    cost_graph(train_loss_list,title="SAE Train graph")
     view_images(train_output,num_epochs)
-    #testing
-    cost_graph(test_loss_list,title="CAE Test graph")
+    
+    cost_graph(test_loss_list,title="SAE Test graph")
     view_images(test_output,num_epochs)
-    FILE = "SAE_kl_model.pth"
+    FILE = "SAE_kl_model.pth"    #path where model is saved
     torch.save(model, FILE)
     
 main()
