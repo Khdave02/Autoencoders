8cf50fcd68e0e5705cf3bc79eaf6705f0c01bf43
Khdave02 <khushi@Khushi-Dave-LAPTOP>
2022-02-13 20:33:23 +0530

    Adding comments in loader.py

--- SAE_L1/loader.py
+++ SAE_L1/loader.py
@@ -3,18 +3,18 @@ import torchvision
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-
+#importing mnist dataset
 def trainLoader(batch_size):
     transform=transforms.ToTensor()
     train_data = datasets.MNIST(
         root="~/torch_datasets",
-        train=True,
+        train=True,                        #train data set 
         download=True,
-        transform=transform
+        transform=transform              #to transform to tensors
     )
     train_loader=DataLoader(train_data, batch_size=batch_size, shuffle=True)
     return train_loader
-
+#here batch size is no of batches in dataset
 def testLoader(batch_size):
     transform=transforms.ToTensor()
     test_data = datasets.MNIST(
