e29731174f949a6a2d734c62b1eef3ec836cea87
Khdave02 <87975841+Khdave02@users.noreply.github.com>
2022-07-17 11:11:00 +0530

    Create loader.py

---
+++ K_Sparse_AE/loader.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+def trainLoader(batch_size):
+    '''
+    loads the training MNIST dataset
+    param batch_size: batch size to be used during training
+    return: DataLoader object of the train set
+    '''
+    transform=transforms.ToTensor()
+    train_data = datasets.MNIST(
+        root="~/torch_datasets",
+        train=True,
+        download=True,
+        transform=transform
+    )
+    train_loader=DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    return train_loader
+
+def testLoader(batch_size):
+    '''
+    loads the testing MNIST dataset
+    param batch_size: batch size to be used during testing
+    return: DataLoader object of the test set
+    '''
+    transform=transforms.ToTensor()
+    test_data = datasets.MNIST(
+        root="~/torch_datasets",
+        train=False,
+        download=True,
+        transform=transform
+    )  
+    test_loader=DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    return test_loader
